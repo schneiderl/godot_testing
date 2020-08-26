f96d07732663851829be33120efc96189c48e98f
Schneider <lucas.schneider@sap.com>
2020-08-26 14:37:16 -0300

    player: added attack animations

--- Player/player.gd
+++ Player/player.gd
@@ -4,27 +4,45 @@ const FRICTION = 500
 const ACCELERATION = 500
 const MAX_SPEED = 80
 
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
 
+var state = MOVE
 var velocity = Vector2.ZERO
 
-onready var animationPlayer = $AnimationPlayer 
+onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
 
 func _physics_process(delta):
-	process_movement(delta)
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+		ROLL:
+			roll_state(delta)
+	
 
-func process_movement(delta):
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized() 
+	input_vector = input_vector.normalized()
 	
 	
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/idle/blend_position", input_vector)
 		animationTree.set("parameters/run/blend_position", input_vector)
+		animationTree.set("parameters/attack/blend_position", input_vector)
 		animationState.travel("run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -32,3 +50,16 @@ func process_movement(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
+
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("attack")
+	
+func roll_state(delta):
+	pass
+		
+
+func attack_animation_finished():
+	state = MOVE
