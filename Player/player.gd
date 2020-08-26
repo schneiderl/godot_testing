db33b1f90fcf1f778a39383a52789dc5ec1c8abc
Schneider <lucas.schneider@sap.com>
2020-08-26 10:12:11 -0300

    initial commit

---
+++ Player/player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+const FRICTION = 500
+const ACCELERATION = 500
+const MAX_SPEED = 80
+
+
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer 
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+
+func _physics_process(delta):
+	process_movement(delta)
+
+func process_movement(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized() 
+	
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/idle/blend_position", input_vector)
+		animationTree.set("parameters/run/blend_position", input_vector)
+		animationState.travel("run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity = move_and_slide(velocity)
