6a7038703c049bdc3e171ec53496b9d133639bc4
Schneider <lucas.schneider@sap.com>
2020-08-28 12:23:06 -0300

    enemy: added bat

--- Player/player.gd
+++ Player/player.gd
@@ -17,11 +17,14 @@ var roll_vector = Vector2.DOWN
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitBox = $HitboxPivot/SwordHitbox
 
 func _ready():
 	animationTree.active = true
 
 func _process(delta):
+	map_inputs(delta)
+	
 	match state:
 		MOVE:
 			move_state(delta)
@@ -30,16 +33,15 @@ func _process(delta):
 		ROLL:
 			roll_state(delta)
 	
-
-func move_state(delta):
+func map_inputs(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
-	
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
+		swordHitBox.knockback_vector = input_vector
 		animationTree.set("parameters/idle/blend_position", input_vector)
 		animationTree.set("parameters/run/blend_position", input_vector)
 		animationTree.set("parameters/attack/blend_position", input_vector)
@@ -54,10 +56,11 @@ func move_state(delta):
 		state = ATTACK
 	if Input.is_action_just_pressed("roll"):
 		state = ROLL
+
+
+func move_state(delta):
 	move()
 	
-
-
 func attack_state(delta):
 	velocity = Vector2.ZERO
 	animationState.travel("attack")
